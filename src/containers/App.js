389345b2003bb1228359db24d2a7ea2ed225b8dd
agnostics <brandilla1337@gmail.com>
2018-05-29 19:00:51 -0400

    Initial Commit

---
+++ src/containers/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./style.scss";
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>PDF Print!</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
